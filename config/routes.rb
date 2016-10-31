27cb2dbdc31138bf7704af9cd18564db2cb3e92d
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-10-31 15:19:02 +0000

    commit before creating new experimental branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require "api_constraints"
 
 Rails.application.routes.draw do
   devise_for :clients
+
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: "api" }, path: "/" do
 
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
                     resources :requests, only: [:index, :show, :create, :update, :destroy]
                   end
                   resources :sessions, only: [:create, :destroy]
+
   end
 
   end
