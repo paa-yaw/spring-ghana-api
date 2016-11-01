c7e78da8d81f6fbfbef723b64e9f67f6c1b35f42
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-11-01 22:05:23 +0000

    adds simple searchto model to be made available to admin later on, all corresponding Rspec tests are green :)

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,23 @@ class Request < ActiveRecord::Base
    
   validates :bedrooms, :bathrooms, :living_rooms, :kitchens, :time_of_arrival, :schedule, :client_id, presence: true
   validates :bedrooms, :bathrooms, :living_rooms, :kitchens, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :filter_by_bedroom_number, lambda { |number| 
+    where("bedrooms == ?", number)
+  }
+
+   scope :filter_by_bathroom_number, lambda { |number| 
+     where("bathrooms == ?", number)
+   }
+
+   scope :filter_by_kitchen_number, lambda {  |number|
+     where("kitchens == ?", number)
+   }
+
+   scope :filter_by_living_room_number, lambda {  |number|
+     where("living_rooms == ?", number)
+   }
+
+   scope :recent, lambda { |arrival|
+     where("time_of_arrival == ?", arrival) }
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -27,4 +27,80 @@ RSpec.describe Request, type: :model do
   # association specs
 
   it { should belong_to :client }
+
+  describe ".filter_by_bedroom_number" do
+    before do 
+      @request1 = FactoryGirl.create :request, bedrooms: 5
+      @request2 = FactoryGirl.create :request, bedrooms: 6
+      @request3 = FactoryGirl.create :request, bedrooms: 7
+      @request4 = FactoryGirl.create :request, bedrooms: 8
+    end 
+
+    context "return requests by bedrooms" do 
+      it "as an array" do
+        expect(Request.filter_by_bedroom_number(7).sort).to match_array([@request3]) 
+      end
+    end
+  end
+
+  describe ".filter_by_bathroom_number" do 
+    before do 
+      @request1 = FactoryGirl.create :request, bathrooms: 2
+      @request2 = FactoryGirl.create :request, bathrooms: 3
+      @request3 = FactoryGirl.create :request, bathrooms: 4
+      @request4 = FactoryGirl.create :request, bathrooms: 5
+    end
+
+    context "returns requests by bathrooms" do 
+      it "as an array" do 
+        expect(Request.filter_by_bathroom_number(5).sort).to match_array([@request4])
+      end
+    end
+  end
+
+  describe ".filter_by_kitchen_number" do 
+    before do
+      @request1 = FactoryGirl.create :request, kitchens: 1
+      @request2 = FactoryGirl.create :request, kitchens: 2
+      @request3 = FactoryGirl.create :request, kitchens: 3
+      @request4 = FactoryGirl.create :request, kitchens: 4
+    end
+
+    context "returns requests by kitchen number" do 
+      it "as an array" do
+        expect(Request.filter_by_kitchen_number(4).sort).to match_array([@request4])
+      end
+    end
+  end
+
+  describe ".filter_by_living_room_number" do 
+    before do
+      @request1 = FactoryGirl.create :request, living_rooms: 1
+      @request2 = FactoryGirl.create :request, living_rooms: 2
+      @request3 = FactoryGirl.create :request, living_rooms: 3
+      @request4 = FactoryGirl.create :request, living_rooms: 4
+    end
+
+    context "returns requests by kitchen number" do 
+      it "as an array" do
+        expect(Request.filter_by_living_room_number(4).sort).to match_array([@request4])
+      end
+    end
+  end
+
+  describe ".filter_by_time_of_arrival" do 
+    before do
+      @time = Time.now
+      @request1 = FactoryGirl.create :request, time_of_arrival: (DateTime.now) - rand(365).days 
+      @request2 = FactoryGirl.create :request, time_of_arrival: (DateTime.now) - rand(365).days 
+      @request3 = FactoryGirl.create :request, time_of_arrival: (DateTime.now) - rand(365).days  
+      @request4 = FactoryGirl.create :request, time_of_arrival: @time
+    end
+
+    context "returns requests by kitchen number" do 
+      it "as an array" do
+        expect(Request.recent(@time).sort).to match_array([@request4])
+      end
+    end
+  end
 end
